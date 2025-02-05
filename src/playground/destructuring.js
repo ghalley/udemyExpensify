ac38a78ea161e255a1ec220d6af1c1cb9a8b3a3d
Gabriel Halley <gabhalley@gmail.com>
2025-02-05 14:09:10 -0500

    10-89 ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,6 @@
+// Object Destructuring
+
+
 // const person = {
 //   name: 'Andrew',
 //   age: 27,
@@ -15,13 +18,26 @@
 //   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin'
-  }
-}
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin'
+//   }
+// }
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
-console.log(publisherName);
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+// console.log(publisherName);
+
+// Array Destructuring
+
+const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+
+const [, city, state = 'New York'] = address;
+console.log(`You are in ${city}, ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [itemName, , mediumPrice] = item;
+
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
