4f8c924c0e443fa4c80a59e077641bb1b0c0b87e
Gabriel Halley <gabhalley@gmail.com>
2025-02-07 10:48:51 -0500

    10-91 Reducers

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -28,7 +28,9 @@ const setCount = ({ count }) => {
   };
 };
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+
+const countReducer = (state = { count: 0 }, action) => {
   switch (action.type) {
     case "INCREMENT":
       return {
@@ -49,7 +51,9 @@ const store = createStore((state = { count: 0 }, action) => {
     default:
       return state;
   }
-});
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
