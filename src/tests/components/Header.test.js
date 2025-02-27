6cab77dce57c6f8eb821ecb68cfaff26be0158c0
Gabriel Halley <gabhalley@gmail.com>
2025-02-27 13:18:41 -0500

    12-119 Enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,9 +1,13 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import React from 'react';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper).toMatchSnapshot();
+  // expect(wrapper.find('h1').length).toBe(1);
+  // const renderer = new ReactShallowRenderer();
+  // renderer.render(<Header />);
+  // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
