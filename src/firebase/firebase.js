b83c5d8833b3f743a85c89f229182f21b34d936c
Gabriel Halley <gabhalley@gmail.com>
2025-03-03 10:11:40 -0500

    14-144 ES6 Promises

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,27 @@
-// import * as firebase from 'firebase';
+import * as firebase from 'firebase';
 
-// const config = {
-// };
+const config = {
 
-// firebase.initializeApp(config);
+};
 
-// const database = firebase.database();
-// database.ref().set({
-//   name: 'Gaby Halley',
-//   age: 39,
-//   isSingle: false,
-//   location: {
-//     city: 'Atlanta',
-//     country: 'United States'
-//   }
-// });
+firebase.initializeApp(config);
 
-// // database.ref().set('This is my data.');
-// database.ref('age').set(37);
+const database = firebase.database();
+database.ref().set({
+  name: 'Gaby Halley',
+  age: 39,
+  isSingle: false,
+  location: {
+    city: 'Atlanta',
+    country: 'United States'
+  }
+});
 
-// database.ref('location/city').set('New York');
+database.ref('age').set(37);
 
-// database.ref('attributes').set({
-//   height: 73,
-//   weight: 185
-// })
+database.ref('location/city').set('New York');
+
+database.ref('attributes').set({
+  height: 73,
+  weight: 185
+})
